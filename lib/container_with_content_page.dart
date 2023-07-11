4dff5d0a11bd0cad5c1151d45d8fe3ab346b5608
michandtke <Michael.Wandtke@hey.com>
2023-07-11 10:24:36 +0200

    Start implementing provider pattern

--- lib/container_with_content_page.dart
+++ lib/container_with_content_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rescuenet_warehouse/container_with_content_column.dart';
 import 'package:rescuenet_warehouse/data_mocks.dart';
 
 import 'menu.dart';
+import 'rescue_container.dart';
+import 'store.dart';
 
 class ContainerWithContentPage extends StatelessWidget {
   @override
@@ -10,14 +13,18 @@ class ContainerWithContentPage extends StatelessWidget {
     return Scaffold(
         body: Column(children: [
       Menu(),
-      Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        ContainerWithContentColumn(
-            container_office, {item_fire: 2, item_chair: 13, item_desk: 1}),
-        ContainerWithContentColumn(
-            container_power, {item_generator: 1, item_fire: 2}),
-        ContainerWithContentColumn(container_medical,
-            {item_cooler: 1, item_para: 1000, item_splint: 1})
-      ])
+      Consumer<Store>(builder: (ctxt, store, _) => _body(store.containers))
     ]));
+  }
+
+  Widget _body(List<RescueContainer> containers) {
+    return Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      ContainerWithContentColumn(
+          containers[0], {item_fire: 2, item_chair: 13, item_desk: 1}),
+      ContainerWithContentColumn(
+          containers[1], {item_generator: 1, item_fire: 2}),
+      ContainerWithContentColumn(
+          containers[2], {item_cooler: 1, item_para: 1000, item_splint: 1})
+    ]);
   }
 }
