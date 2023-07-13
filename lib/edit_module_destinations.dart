a98b6d428ad18851576ecda80325a5961e52bd41
michandtke <Michael.Wandtke@hey.com>
2023-07-13 16:32:43 +0200

    CUD of module destinations

--- lib/edit_module_destinations.dart
+++ lib/edit_module_destinations.dart
@@ -7,7 +7,14 @@ import 'menu.dart';
 import 'rescue_text.dart';
 import 'store.dart';
 
-class EditModuleDestinations extends StatelessWidget {
+class EditModuleDestinations extends StatefulWidget {
+  @override
+  State createState() => _EditModuleDestinationsState();
+}
+
+class _EditModuleDestinationsState extends State<EditModuleDestinations> {
+  final TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,19 +31,58 @@ class EditModuleDestinations extends StatelessWidget {
             builder: (ctx, store, _) => _table(store.moduleDestinations)));
   }
 
-  Widget _table(List<String> moduleDestinations) =>
-      RescueTable(const ["Name", ""], _rows(moduleDestinations), {});
+  Widget _table(List<String> moduleDestinations) => RescueTable(
+      const ["Name", ""], [..._rows(moduleDestinations), _addingRow()], {});
 
   List<TableRow> _rows(List<String> moduleDestinations) =>
       moduleDestinations.map((e) => _buildRow(e)).toList();
 
-  TableRow _buildRow(String destination) => TableRow(
-      children: [
+  TableRow _buildRow(String destination) => TableRow(children: [
         Padding(
-            padding: const EdgeInsets.only(left: 20, top: 16, bottom: 16),
-            child: RescueText.normal(destination)),
-        _deleteBtn()
+            padding: const EdgeInsets.only(left: 20),
+            child: _text(destination)),
+        _btnDelete(destination)
       ]);
 
-  _deleteBtn() => const RescueText(24, '-', FontWeight.w700);
+  _text(String destination) => _textField(
+      TextEditingController(text: destination),
+      (newText) => _changeText(destination, newText));
+
+  _changeText(String oldDest, String newDest) =>
+      Provider.of<Store>(context, listen: false)
+          .editDestination(oldDest, newDest);
+
+  _btnDelete(String text) => TextButton(
+      onPressed: () {
+        Provider.of<Store>(context, listen: false).removeDestination(text);
+      },
+      child: const RescueText(24, '-', FontWeight.w700));
+
+  TableRow _addingRow() => TableRow(children: [
+        Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: _textField(_controller)),
+        _btnAdd()
+      ]);
+
+  _btnAdd() => TextButton(
+      onPressed: () {
+        Provider.of<Store>(context, listen: false)
+            .addDestination(_controller.text);
+        _controller.clear();
+      },
+      child: const RescueText(24, '+', FontWeight.w700));
+
+  Widget _textField(TextEditingController controller,
+          [ValueChanged<String>? onChange]) =>
+      TextField(
+        style: const TextStyle(fontSize: 24),
+        decoration: const InputDecoration(
+            hintText: "Insert new destination here",
+            hintStyle: TextStyle(fontSize: 24)),
+        controller: controller,
+        onTapOutside: (_) {
+          if (onChange != null) onChange(controller.text);
+        },
+      );
 }
--- lib/store.dart
+++ lib/store.dart
@@ -54,6 +54,28 @@ class Store extends ChangeNotifier {
   UnmodifiableListView<String> get moduleDestinations =>
       UnmodifiableListView(_moduleDestinations);
 
+  addDestination(String? text) {
+    if (text != null && !_moduleDestinations.contains(text)) {
+      _moduleDestinations.add(text);
+      notifyListeners();
+    }
+  }
+
+  removeDestination(String? text) {
+    if (_moduleDestinations.contains(text)) {
+      _moduleDestinations.remove(text);
+      notifyListeners();
+    }
+  }
+
+  editDestination(String oldDest, String newDest) {
+    var idx = _moduleDestinations.indexOf(oldDest);
+    if (idx != -1) {
+      _moduleDestinations.replaceRange(idx, idx + 1, [newDest]);
+      notifyListeners();
+    }
+  }
+
   UnmodifiableListView<String> get currentLocations =>
       UnmodifiableListView(_currentLocations);
 
