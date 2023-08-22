a1d3b46b4553d285530fdd358e143de08ba5fdf1
michandtke <Michael.Wandtke@hey.com>
2023-08-22 08:15:50 +0200

    Start adding firebase to item store

--- lib/item_store.dart
+++ lib/item_store.dart
@@ -1,6 +1,7 @@
 import 'dart:collection';
 import 'dart:math';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:rescuenet_warehouse/main.dart';
 
@@ -8,6 +9,7 @@ import 'data_mocks.dart';
 import 'item.dart';
 
 class ItemStore extends ChangeNotifier {
+  static const String _firebasePath = 'items';
   final List<Item> _items = [
     item_fire,
     item_chair,
@@ -17,6 +19,23 @@ class ItemStore extends ChangeNotifier {
     item_para,
     item_splint
   ];
+
+  // void listenToRNItemsFromFB() {
+  //   FirebaseFirestore.instance
+  //       .collection(_firebasePath)
+  //       .snapshots()
+  //       .listen((QuerySnapshot<Map<String, dynamic>> querySnapshot) {
+  //     List<SingleRNItemModel> _rnIncome = querySnapshot.docs
+  //         .map((QueryDocumentSnapshot<Map<String, dynamic>> e) =>
+  //         SingleRNItemModel.fromJson(e.data(), e.id))
+  //         .toList();
+  //     _rnItems = _rnIncome.where((element) => element.isBox == false).toList();
+  //     _rnBoxes = _rnIncome.where((element) => element.isBox).toList();
+  //     print("RNItems from Firebase: ${_rnItems.length}");
+  //     print("RNBoxes from Firebase: ${_rnBoxes.length}");
+  //     notifyListeners();
+  //   });
+  // }
 
   UnmodifiableListView<Item> get items => UnmodifiableListView(_items);
 
